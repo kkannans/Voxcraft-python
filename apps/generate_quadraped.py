a3433e3390752358337c8d22413589d6ba0833a9
Krishna Kannan Srinivasan <kkannans@uvm.edu>
2022-08-05 16:54:11 -0400

    init

--- apps/generate_quadraped.py
+++ apps/generate_quadraped.py
@@ -1,31 +1,49 @@
 import numpy as np
-import os
-from modules.VoxcraftVXA import VXA
-from modules.VoxcraftVXD import VXD
+from dataclasses import dataclass
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-data_path = os.getcwd() + "/data"
-if not os.path.exists(data_path):
-    os.makedirs(data_path)
-# Generate a Base VXA file
-# See here for list of vxa tags: https://gpuvoxels.readthedocs.io/en/docs/
-vxa = VXA(EnableExpansion=1, SimTime=5) # pass vxa tags in here
+def plot_voxels(array_3d: np.array):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_aspect('auto')
+    ax.voxels(array_3d, edgecolor="k")
+    plt.show()
 
-# Create two materials with different properties
-mat1 = vxa.add_material(RGBA=(255,0,255), E=5e4, RHO=1e4) # returns the material ID
-mat2 = vxa.add_material(RGBA=(255,0,0), E=1e8, RHO=1e4)
+@dataclass
+class vector:
+    x:float
+    y:float
+    z:float
 
-# Write out the vxa to data/ directory
-base_vxa_path = data_path + "/base.vxa"
-vxa.write(base_vxa_path)
+grid_size = 10
+target = np.zeros(shape=(grid_size,grid_size,grid_size), dtype = int)
+torso_dim = vector(3.0, 2.0, 1.0)
+leg_dim = vector(1.0, 1.0, 2.0)
+torso_origin = vector(grid_size//2, grid_size//2, torso_dim.z + leg_dim.z)
 
-# Create random body array between 0 and maximum material ID
-body = np.random.randint(0,mat2+1,size=(20,20,20))
+def make_torso(target:np.array, torso_origin:vector, torso_dim:vector):
+    start_x = int(torso_origin.x - torso_dim.x/2.0)
+    end_x = int(torso_origin.x + torso_dim.x/2.0)
+    start_y = int(torso_origin.y - torso_dim.y/2.0)
+    end_y = int(torso_origin.y + torso_dim.y/2.0)
+    start_z = int(torso_origin.z - torso_dim.z/2.0)
+    end_z = int(torso_origin.z + torso_dim.z/2.0)
+    print(start_x ,end_x)
+    print(start_y ,end_y)
+    print(start_z ,end_z)
+    target[start_x : end_x, start_y : end_y, start_z : end_z] = 1
+    return target
+
+def add_legs(target:np.array, leg_dim:vector):
+    # get 4 corner indices of the torso
+    min_coordinate = np.min(target == 1)
+    max_coordinate = np.max(target == 1)
+    print(min_coordinate, max_coordinate)
+    front_left = vector() 
+    return target
 
 
-# Generate a VXD file (to configure a single simulation)
-vxd = VXD()
-vxd.set_tags(RecordVoxel=1) # pass vxd tags in here to overwite vxa tags
-vxd.set_data(body)
-# Write out the vxd to data/
-base_vxd_path = data_path+"/robot.vxd"
-vxd.write(base_vxd_path)
+target = make_torso(target, torso_origin, torso_dim)
+plot_voxels(target)
