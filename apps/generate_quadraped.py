680ca9b669c49f37dcca9bb6e6c0ade2d768aa2b
kkannans <kkannans@uvm.edu>
2022-08-05 18:25:24 -0600

    quadraped simple design

--- apps/generate_quadraped.py
+++ apps/generate_quadraped.py
@@ -11,39 +11,11 @@ def plot_voxels(array_3d: np.array):
     ax.voxels(array_3d, edgecolor="k")
     plt.show()
 
-@dataclass
-class vector:
-    x:float
-    y:float
-    z:float
-
+# make quadraped by removing voxels from a cube
 grid_size = 10
-target = np.zeros(shape=(grid_size,grid_size,grid_size), dtype = int)
-torso_dim = vector(3.0, 2.0, 1.0)
-leg_dim = vector(1.0, 1.0, 2.0)
-torso_origin = vector(grid_size//2, grid_size//2, torso_dim.z + leg_dim.z)
-
-def make_torso(target:np.array, torso_origin:vector, torso_dim:vector):
-    start_x = int(torso_origin.x - torso_dim.x/2.0)
-    end_x = int(torso_origin.x + torso_dim.x/2.0)
-    start_y = int(torso_origin.y - torso_dim.y/2.0)
-    end_y = int(torso_origin.y + torso_dim.y/2.0)
-    start_z = int(torso_origin.z - torso_dim.z/2.0)
-    end_z = int(torso_origin.z + torso_dim.z/2.0)
-    print(start_x ,end_x)
-    print(start_y ,end_y)
-    print(start_z ,end_z)
-    target[start_x : end_x, start_y : end_y, start_z : end_z] = 1
-    return target
-
-def add_legs(target:np.array, leg_dim:vector):
-    # get 4 corner indices of the torso
-    min_coordinate = np.min(target == 1)
-    max_coordinate = np.max(target == 1)
-    print(min_coordinate, max_coordinate)
-    front_left = vector() 
-    return target
-
-
-target = make_torso(target, torso_origin, torso_dim)
+target = np.zeros(shape = (grid_size,grid_size,grid_size),dtype = int)
+body_span = 5
+target[:body_span, :body_span, :body_span+2] = 1
+target[1:body_span-1, :, :body_span - 1] = 0
+target[:,1:body_span-1, :body_span - 1] = 0
 plot_voxels(target)
